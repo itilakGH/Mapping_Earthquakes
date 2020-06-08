e512312afc256ca7d49e09ea816bddd87087436d
itilakGH <irentilak@gmail.com>
2020-06-08 16:02:54 -0700

    Adding Toronto routes changed code

--- Mapping_GeoJSON_Linestrings/static/js/logic.js
+++ Mapping_GeoJSON_Linestrings/static/js/logic.js
@@ -1,5 +1,5 @@
 // We create the tile layer that will be the background of our map.
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/outdoors-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let light = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
 	maxZoom: 18,
 	accessToken: API_KEY
@@ -12,14 +12,14 @@ attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap
 });
 // Create a base layer that holds both maps.
 let baseMaps = {
-	Street: streets,
+	Light: light,
 	Dark: dark
   };
 // Create the map object with center, zoom level and default layer.
 let map = L.map('mapid', {
-	center: [30, 30],
-	zoom: 2,
-	layers: [streets]
+	center: [31.79, 7.09],
+	zoom: 3,
+	layers: [light]
 });
 
 
@@ -28,18 +28,19 @@ L.control.layers(baseMaps).addTo(map);
 // Then we add our 'graymap' tile layer to the map.
 //streets.addTo(map);
 
-// Accessing the airport GeoJSON URL
-let airportData = "https://raw.githubusercontent.com/itilakGH/Mapping_Earthquakes/Mapping_GeoJSON_Points/majorAirports.json";
+// Accessing the Toronto airline routes GeoJSON URL
+let torontoData = "https://raw.githubusercontent.com/itilakGH/Mapping_Earthquakes/Mapping_GeoJSON_Linestrings/torontoRoutes.json";
 // Grabbing our GeoJSON data.
-d3.json(airportData).then(function(data) {
+d3.json(torontoData).then(function(data) {
     console.log(data);
   // Creating a GeoJSON layer with the retrieved data.
   L.geoJson(data, {
+		// Adding a style
+		color: "yellow",
+		weight: 0.5,
 	  // We turn each feature into a marker on the map.
 	  onEachFeature: function(feature, layer) {
-		console.log(layer);
-	   
-		layer.bindPopup("<h3>" + feature.properties.faa + "</h3> <hr> <h5> Airport name: " + feature.properties.name + "</h5>");
+		layer.bindPopup("<h3>" + feature.properties.airline + "</h3> <hr> <h5> Route: " + feature.properties.src + "-->" + feature.properties.dst + "</h5>");
   }}).addTo(map);
 });
 
